141e7c5a83cc1b0172790a97a4569dd7e40b21c2
Vanskool15 <darrellnew2018@gmail.com>
2024-03-07 14:43:10 +0800

    Update clockdate.js

--- clockdate.js
+++ clockdate.js
@@ -44,8 +44,8 @@ month = month.toString().padStart(2, '0');
 date = date.toString().padStart(2, '0');
 
 
-document.getElementById('month').innerHTML =  month;
-document.getElementById('date').innerHTML = date;
+document.getElementById('month').innerHTML =  month + " | ";
+document.getElementById('date').innerHTML = date + " | ";
 document.getElementById('year').innerHTML = year;
 document.getElementById('day').innerHTML = day;
 
