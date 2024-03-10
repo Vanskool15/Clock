04fff6247ac55f28cfe8970cd6957968ee41a432
Vanskool15 <darrellnew2018@gmail.com>
2024-03-10 17:18:02 +0800

    update comment

--- clockdate.js
+++ clockdate.js
@@ -40,7 +40,7 @@ function dateMonth() {
   let date = d.getDate();
   let day = weekDay [d.getDay()];
 
-//Add zero before date & month
+//Convert  date & month to string
 month = month.toString().padStart(2, '0');
 date = date.toString().padStart(2, '0');
 
