92e0d920adbb3516f77ccc92e7c05eed0f1dee19
Vanskool15 <darrellnew2018@gmail.com>
2024-03-14 17:13:56 +0800

    add javascript

--- clockdate.js
+++ clockdate.js
@@ -1,3 +1,32 @@
+
+setInterval(setClock,1000);
+
+const hourHand = document.querySelector(`[data-hour-hand]`);
+const minuteHand = document.querySelector(`[data-minute-hand]`);
+const secondHand = document.querySelector(`[data-second-hand]`)
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio +currentDate.getHours()) / 12;
+
+    setRotation(secondHand,secondsRatio);
+    setRotation(minuteHand,minutesRatio);
+    setRotation(hourHand,hoursRatio);
+
+}
+
+
+
+function setRotation(element, rotationRatio) {
+element.style.setProperty(`--rotation`, rotationRatio * 360)
+}
+
+
+
+
+
 const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
@@ -197,34 +226,9 @@ window.onload = function (){
   updateNewZealandTime();
   updateOttawaTime();
   dateMonth();
+  setClock();
   
 
 };
 
 
-
-setInterval(setClock,1000);
-
-const hourHand = document.querySelector(`[data-hour-hand]`);
-const minuteHand = document.querySelector(`[data-minute-hand]`);
-const secondHand = document.querySelector(`[data-second-hand]`)
-
-function setClock() {
-    const currentDate = new Date();
-    const secondsRatio = currentDate.getSeconds() / 60;
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-    const hoursRatio = (minutesRatio +currentDate.getHours()) / 12;
-
-    setRotation(secondHand,secondsRatio);
-    setRotation(minuteHand,minutesRatio);
-    setRotation(hourHand,hoursRatio);
-
-}
-
-
-
-function setRotation(element, rotationRatio) {
-element.style.setProperty(`--rotation`, rotationRatio * 360)
-}
-
-setClock();
